0539be7c7c5a60c07bb69b6acbd1e9d2198152ef
Shrey Patel <76216765+shrey714@users.noreply.github.com>
2024-08-20 19:01:02 +0530

    Update server.js

--- functions/server.js
+++ functions/server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 
 app.use(
 cors({
-origin: "http://localhost:3000/",
+origin: "http://localhost:3000/documents/pricing",
 credentials: true,
 methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
 allowedHeaders: [
